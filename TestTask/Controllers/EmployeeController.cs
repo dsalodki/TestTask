124b0eb87aabf4969a8f2fc228f167bbad8e11de
Salodki Dzmitry Piatrovich <dsalodki@gmail.com>
2021-09-14 14:03:15 +0300

    json file uploaded and processed. return txt log

--- TestTask/Controllers/EmployeeController.cs
+++ TestTask/Controllers/EmployeeController.cs
@@ -3,8 +3,14 @@ using DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using TestTask.Models;
 
 namespace TestTask.Controllers
 {
@@ -53,6 +59,10 @@ namespace TestTask.Controllers
             {
                 employee.PersonnelNumber = model.Id;
             }
+            else
+            {
+                employee.PersonnelNumber = null;
+            }
             _db.SaveChanges();
 
             return new JsonResult(new { success = true });
@@ -62,7 +72,7 @@ namespace TestTask.Controllers
         public ActionResult Delete(int id)
         {
             var employee = _db.Employees.FirstOrDefault(x => x.Id == id);
-            if(employee == null)
+            if (employee == null)
             {
                 return new JsonResult(new { success = false, errorMsg = "can not find such Employee" });
             }
@@ -88,5 +98,91 @@ namespace TestTask.Controllers
             return new JsonResult(new { success = true });
         }
 
+        public IActionResult UploadJson(IFormFile jsonFile)
+        {
+            EmployeeJsonData[] json = null;
+            // read json
+            using (var reader = new StreamReader(jsonFile.OpenReadStream()))
+            {
+                var content = reader.ReadToEnd();
+                json = JsonConvert.DeserializeObject<EmployeeJsonData[]>(content);
+            }
+
+            // update db
+            if (json == null)
+            {
+                return File(new byte[] { }, "text/plain");
+            }
+
+            var result = new StringBuilder();
+            foreach (var row in json)
+            {
+                if (string.IsNullOrEmpty(row.FullName))
+                {
+                    result.AppendLine($"error: Employee with id {row.Id} has empty Fullname, skipped");
+                    continue;
+                }
+
+                if (!row.IsRegular && row.PersonnelNumber.HasValue)
+                {
+                    result.AppendLine(
+                        $"error: not regular employee can not have PersonnelNumber {row.PersonnelNumber.HasValue}");
+                    row.PersonnelNumber = null;
+                }
+
+                if (row.IsRegular && !row.PersonnelNumber.HasValue)
+                {
+                    result.AppendLine($"error: regular employee with Id {row.Id} has not PersonnelNumber");
+                    if (row.Id != 0)
+                    {
+                        row.PersonnelNumber = row.Id;
+                    }
+                }
+
+                // add entry to db
+                if (row.Id == 0)
+                {
+                    var employee = new Employee()
+                    {
+                        IsRegular = row.IsRegular,
+                        FullName = row.FullName,
+                        Birthday = row.Birthday,
+                        Sex = (DataLayer.Entities.Sex)row.Sex
+                    };
+                    _db.Employees.Add(employee);
+                    _db.SaveChanges();
+                    if (employee.IsRegular)
+                    {
+                        employee.PersonnelNumber = employee.Id;
+                        _db.SaveChanges();
+                    }
+
+                    result.AppendLine($"Entry with Id {employee.Id} was created");
+                }
+                else
+                {
+                    var employee = _db.Employees.FirstOrDefault(x => x.Id == row.Id);
+                    if (employee == null)
+                    {
+                        result.AppendLine($"error: Employee with Id {row.Id} was not find");
+                    }
+                    else
+                    {
+                        employee.IsRegular = row.IsRegular;
+                        employee.PersonnelNumber = row.PersonnelNumber;
+                        employee.Birthday = row.Birthday;
+                        employee.FullName = row.FullName;
+                        employee.Sex = (DataLayer.Entities.Sex)row.Sex;
+
+                        _db.SaveChanges();
+
+                        result.AppendLine($"Entry with Id {row.Id} was changed");
+                    }
+                }
+            }
+
+
+            return File(Encoding.UTF8.GetBytes(result.ToString()), "text/plain");
+        }
     }
 }
---
+++ TestTask/Models/EmployeeJsonData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTask.Models
+{
+    public class EmployeeJsonData
+    {
+        public int Id { get; set; }
+        public int? PersonnelNumber { get; set; }
+        public string FullName { get; set; }
+        public Sex Sex { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool IsRegular { get; set; }
+    }
+
+    public enum Sex
+    {
+        Male = 0,
+        Female = 1
+    }
+}
