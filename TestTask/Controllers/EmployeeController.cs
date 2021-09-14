75b8f4a42da5aeeb9462d51c1db4fe8fa97ac374
Salodki Dzmitry Piatrovich <dsalodki@gmail.com>
2021-09-14 10:26:53 +0300

    grid filled , add new employee works

--- TestTask/Controllers/EmployeeController.cs
+++ TestTask/Controllers/EmployeeController.cs
@@ -17,6 +17,11 @@ namespace TestTask.Controllers
             _db = db;
         }
 
+        public IActionResult GetTotal()
+        {
+            return new JsonResult(_db.Employees.Count());
+        }
+
         [HttpPost]
         public IActionResult GetEmployees(int page, int rows)
         {
@@ -32,9 +37,9 @@ namespace TestTask.Controllers
         }
 
         [HttpPost]
-        public ActionResult Update(int id, Employee model)
+        public ActionResult Update(Employee model)
         {
-            var employee = _db.Employees.FirstOrDefault(x => x.Id == id);
+            var employee = _db.Employees.FirstOrDefault(x => x.Id == model.Id);
             if (employee == null)
             {
                 return new JsonResult(new { success = false, errorMsg = "can not find such Employee" });
@@ -46,7 +51,7 @@ namespace TestTask.Controllers
             employee.IsRegular = model.IsRegular;
             if (employee.IsRegular)
             {
-                employee.PersonnelNumber = id;
+                employee.PersonnelNumber = model.Id;
             }
             _db.SaveChanges();
 
@@ -72,6 +77,9 @@ namespace TestTask.Controllers
         public ActionResult Save(Employee model)
         {
             _db.Employees.Add(model);
+            _db.SaveChanges();
+            var employee = _db.Employees.FirstOrDefault(x => x.Id == model.Id);
+            employee.PersonnelNumber = model.Id;
             _db.SaveChanges();
 
             return new JsonResult(new { success = true });
